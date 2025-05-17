232c1207249f1772200888d13b09132bee34d44f
sheep-programmer <1917961828@qq.com>
2025-05-17 22:40:22 +0800

    MineSweeper Game Project

---
+++ 代码版/StartGame.cpp
@@ -0,0 +1,14 @@
+#include <easyx.h>      // EasyX图形库
+#include <graphics.h>   // EasyX图形库扩展
+#include <windows.h>    // Windows API
+#include "Game.h"
+
+int main() {
+    // 创建扫雷游戏对象
+    Game game;
+    
+    // 开始游戏（按F9键可以显示所有雷的位置）
+    game.start();
+    
+    return 0;
+} 
